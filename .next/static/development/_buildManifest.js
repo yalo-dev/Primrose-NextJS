0149e6d569a611bb139508de83e38e8ee7e66ece
SRI Developer <dev@sriexecutive.com>
2023-09-27 11:03:50 -0400

    resources card hover state.

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/resources":["static\u002Fchunks\u002Fpages\u002Fresources.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fresources"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
