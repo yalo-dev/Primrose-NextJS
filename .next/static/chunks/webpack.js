af036f29b2e0ef385f98b83dc55ad7e068c62c97
Sabrina Matthews <sabrina.matthews@digitalyalo.com>
2023-09-28 16:02:05 -0400

    updating featured tag's green.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f9d75ca2a4756c91"; }
+/******/ 		__webpack_require__.h = function() { return "676761fe8ba840eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
