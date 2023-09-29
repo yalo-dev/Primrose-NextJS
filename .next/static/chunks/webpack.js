79065337edfd0e4b39b0c855dcf6f60101d0a9ba
Sabrina Matthews <sabrina.matthews@digitalyalo.com>
2023-09-29 01:19:40 -0400

    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a671f1415cf5c0a7"; }
+/******/ 		__webpack_require__.h = function() { return "552364980b550d4a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
