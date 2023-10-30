1c625150cafaf1a157020fb8849acfc3fed10589
Sabrina Matthews <sabrina.matthews@digitalyalo.com>
2023-10-30 16:06:00 -0400

    no message

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/[...pageId]":["static\u002Fchunks\u002Fpages\u002F[...pageId].js"],sortedPages:["\u002F_app","\u002F_error","\u002F[...pageId]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
