729ba81823458ed4a2956741d2aa12ec834ad39a
Sabrina Matthews <sabrina.matthews@digitalyalo.com>
2023-11-17 14:46:57 -0500

    updates

--- pages/schools/[schoolSlug]/schedule-a-tour.tsx
+++ pages/schools/[schoolSlug]/schedule-a-tour.tsx
@@ -1,7 +1,41 @@
 export default function ScheduleATourPage() {
     return (
         <section className='module'>
-            SCHEDULE A TOUR
+
+            <div className="main">
+                <div className="container">
+                    <div className="heading">
+                        <h1>
+                            Schedule A Tour
+                        </h1>
+                        <div className="b2">
+                            Lorem ipsum dolor sit amet consectetur. Quam eget hendrerit nisi ultrices egestas ut vitae facilisis phasellus. Pellentesque sed senectus sed turpis eget vitae. Dictum in urna id eleifend nisi vitae.
+                        </div>
+                    </div>
+                </div>
+                <div className="aside">
+                    <div className="address">
+
+                    </div>
+                    <div className="hours">
+
+                    </div>
+                    <div className="phone">
+
+                    </div>
+                    <div className="social">
+                        <div className="h5 green">
+                            Connect with Us
+                        </div>
+                        <div className="fb">
+
+                        </div>
+                        <div className="ig">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
