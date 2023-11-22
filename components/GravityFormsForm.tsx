fe4ecc229308fc30b65352d294c01d29b04b488b
Aisha Henderson <aisha.henderson@digitalyalo.com>
2023-11-22 11:15:19 -0500

    📋Form submission is working: with TEXTFIELD 📝

--- components/GravityFormsForm.tsx
+++ components/GravityFormsForm.tsx
@@ -5,7 +5,7 @@ import useGravityForm from "../hooks/useGravityForm";
 import GravityFormsField from "./GravityFormsField";
 
 const SUBMIT_FORM = gql`
-  mutation submitForm($formId: ID!, $fieldValues: [FormFieldValuesInput]) {
+  mutation submitForm($formId: ID!, $fieldValues: [FormFieldValuesInput]!) {
     submitGfForm(input: {
       id: $formId
       fieldValues: $fieldValues
@@ -31,21 +31,21 @@ interface Props {
 }
 
 export default function GravityFormsForm({ form }: Props) {
-  const [submitGfForm, { data, loading, error }] = useMutation(SUBMIT_FORM);
+  const [submitForm, { data, loading, error }] = useMutation(SUBMIT_FORM);
   const haveEntryId = Boolean(data?.submitGfForm?.entry?.id);
   const haveFieldErrors = Boolean(data?.submitGfForm?.errors?.length);
   const wasSuccessfullySubmitted = haveEntryId && !haveFieldErrors;
   //const defaultConfirmation = form.confirmations?.find(confirmation => confirmation?.isDefault);
-  //const formFields = form.formFields?.nodes || [];
+  const formFields = form?.formFields?.nodes || [];
   const { state } = useGravityForm();
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     if (loading) return;
 
-    submitGfForm({
+    submitForm({
       variables: {
-        id: form.formId,
+        formId: form.formId,
         fieldValues: state,
       }
     }).catch(error => {
@@ -62,12 +62,14 @@ export default function GravityFormsForm({ form }: Props) {
     return <p>{'Form successfully submitted - thank you.'}</p>
   }
 
+  console.log('form: ', form?.formFields?.nodes);
+  console.log('state: ', state);
   return (
     <form method="post" onSubmit={handleSubmit}>
       {form?.title ? <h2>{form.title}</h2> : null}
       {form?.description ? <p>{form.description}</p> : null}
       
-      {form?.formFields?.nodes?.map((field: FormField) => (
+      {formFields.map((field: FormField) => (
         <GravityFormsField
           key={field.id}
           field={field}
