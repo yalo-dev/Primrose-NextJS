60aa06b3a2c1b4b499ab78251ee573d6f3ef701c
Sabrina Matthews <sabrina.matthews@digitalyalo.com>
2023-12-15 11:13:34 -0500

    pushing up a QA fix for the close icon to not overlap text in Gen Horizontal Tabs scss file and adding pos relative to search bar so the clear icon doesn't go outside of parent width.

--- app/components/organisms/Header/Header.tsx
+++ app/components/organisms/Header/Header.tsx
@@ -242,7 +242,7 @@ export default function Header({ menuItems }) {
                     </div>
                 </div>
                 <div ref={desktopSearchBarRef} className={`desktop-search-bar ${isDesktopSearchActive ? 'show' : ''}`}>
-                    <div className="container">
+                    <div className="container position-relative">
                         <div className='search-icon desktop d-none d-lg-flex'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M13.8947 7.81579C13.8947 11.1731 11.1731 13.8947 7.81579 13.8947C4.45848 13.8947 1.73684 11.1731 1.73684 7.81579C1.73684 4.45848 4.45848 1.73684 7.81579 1.73684C11.1731 1.73684 13.8947 4.45848 13.8947 7.81579ZM12.8913 13.7595C11.5257 14.9267 9.75308 15.6316 7.81579 15.6316C3.49925 15.6316 0 12.1323 0 7.81579C0 3.49925 3.49925 0 7.81579 0C12.1323 0 15.6316 3.49925 15.6316 7.81579C15.6316 9.56904 15.0543 11.1875 14.0794 12.4913L17.7284 16.1403L16.5003 17.3685L12.8913 13.7595Z" fill="white" />
