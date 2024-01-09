c8190d64efe60bc948ffea14d1b545e011636c50
Chris Dawson <chris.dawson@digitalyalo.com>
2024-01-09 15:18:38 -0500

    temp remove jobID from graphql query

--- pages/schools/[schoolSlug]/careers/index.tsx
+++ pages/schools/[schoolSlug]/careers/index.tsx
@@ -61,7 +61,7 @@ export async function getServerSideProps(context) {
               jobType
               jobDescription
               postDate
-              jobId
+              
             }
           }
           schoolCorporateSettings {
