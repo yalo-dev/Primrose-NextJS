c10106a7243e1dfac7e2351413d5608c5db596a5
SRI Developer <dev@sriexecutive.com>
2024-01-20 18:18:43 -0500

    re-add bootstrap js

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -119,6 +119,7 @@ export default function Locations({ markets, schools }) {
     return (
         
         <>
+            <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous" />
             <div className='container-fluid bg-gray heading py-5'>
                 <div className='row py-5 '>
                     <div className='col-12 text-center'>
