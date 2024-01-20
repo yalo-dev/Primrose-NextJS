ed46576bdfabb8ba7851a12913eec274b7033432
SRI Developer <dev@sriexecutive.com>
2024-01-20 11:32:36 -0500

    Locations Page with Search

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -5,7 +5,9 @@ import Link from 'next/link';
 import Heading from '../../app/components/atoms/Heading/Heading';
 import Button from '../../app/components/atoms/Button/Button';
 import SelectDropdown from '../../app/components/molecules/SelectDropdown/SelectDropdown';
-
+import bootstrap from 'bootstrap';
+import Script from 'next/script';
+import { Http2ServerResponse } from 'http2';
 
 const slugify = require('slugify');
 
@@ -19,6 +21,9 @@ export async function getStaticProps() {
                     nodes {
                       slug
                       name
+                      markets{
+                        marketState
+                      }
                     }
                   }
                   uri
@@ -40,6 +45,7 @@ export async function getStaticProps() {
                   }
                   name
                   slug
+                  uri
                 }
               }
         }`;
@@ -65,13 +71,21 @@ export async function getStaticProps() {
 }
 
 export default function Locations({ markets, schools }) {
-    
+    const handleStateFilter = (state:string) => {
+        let state_section = document.getElementById(slugify(state, {lower:true}) + "_section");
+        let sections = document.getElementsByClassName('state_section');
+        $(sections).hide();
+        $(state_section).show();
+      };
         
     let schools_arr = [];
     schools.map((school, index) => {
        
         if(school.markets.nodes.length>0){
-            schools_arr[school.markets.nodes[0].name] = school;
+            if(!schools_arr[school.markets.nodes[0].name]){
+                schools_arr[school.markets.nodes[0].name] = []
+            }
+            schools_arr[school.markets.nodes[0].name].push(school);
         }
        
     
@@ -101,18 +115,67 @@ export default function Locations({ markets, schools }) {
     
     
     return (
+        
         <>
-            <div className='container-fluid bg-gray heading'>
+            <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" />
+            <div className='container-fluid bg-gray heading py-5'>
                 <div className='row py-5 '>
                     <div className='col-12 text-center'>
                         <h1>All Locations</h1>
                         <SelectDropdown
                           options={statesOptions}
                           placeholder="States"
+                          onSelect={handleStateFilter}
                           returnFullOption={false}
                         />
                     </div>
                 </div>
+            </div>
+            <div className='container locations py-5'>
+
+                <div className='row py-5'>
+
+                    {states.map((state, i) => (
+                        <>
+                        <div id={slugify(state, {lower:true}) + "_section"} className="state_section">
+                        <span class="subheading" key={i}>{state}</span>
+                        <div className="accordion accordion-flush" id={slugify(state, {lower:true})}>
+                        {markets.map((market, index) => market.markets.marketState == state && (
+                            <>
+                            
+                                <div className="accordion-item">
+
+                                    <h2 className="accordion-header" id={'heading'+index}>
+                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse" + index} aria-expanded="false" aria-controls={"collapse" + index}>
+                                            <h5>{market.name}</h5>
+                                        </button>
+                                    </h2>
+                                    <div id={"collapse" + index} className="accordion-collapse collapse" data-bs-parent={"#" + slugify(state, {lower:true})}>
+                                        <div className="accordion-body">
+                                        {schools_arr[market.name] && schools_arr[market.name].sort() && schools_arr[market.name].map((school, index) => (
+                                            <>
+                                            <a href={school.uri}>{"Primrose School " + school.schoolCorporateSettings.schoolOfAtOn + " " + school.title}</a>
+                                            </>
+                                        ))}
+                                        </div>
+                                    </div>
+
+                                </div>
+                            
+                            </>
+
+                        ))}
+                        </div>
+                        </div>
+                        </>
+                        
+                       
+                    )
+                        )
+                    }
+
+                </div>
+
             </div>
         </>
     );
