3b9d6fa3cb45c7fb715ecfbdf1d8dc86adc14d12
Elijah Williams <williamselijah527@gmail.com>
2024-01-31 18:34:52 -0500

    Harmony Module

--- app/components/modules/HarmonyAndHeart/HarmonyAndHeart.tsx
+++ app/components/modules/HarmonyAndHeart/HarmonyAndHeart.tsx
@@ -37,21 +37,21 @@ interface HarmonyAndHeartProps {
         altText?: string;
         sourceUrl?: string;
       };
-    spotifyLink?: {
-          target?: string;
-          title?: string;
-          url?: string;
-        };
-    appleMusicLink?: {
-            target?: string;
-            title?: string;
-            url?: string;
-          };
-      }[];
+      spotifyLink?: {
+        target?: string;
+        title?: string;
+        url?: string;
+      };
+      appleMusicLink?: {
+        target?: string;
+        title?: string;
+        url?: string;
+      };
       title?: string;
     }[];
     // ... other right column fields
   };
+}
 
 
 const HarmonyAndHeart: React.FC<HarmonyAndHeartProps> = ({ leftColumn, moduleId, rightHarmonyColumn }) => {
@@ -82,7 +82,7 @@ const HarmonyAndHeart: React.FC<HarmonyAndHeartProps> = ({ leftColumn, moduleId,
   }, []);
 
   // Add a CSS class for the hover effect
-const hoverEffectClass = 'hover-effect';
+  const hoverEffectClass = 'hover-effect';
 
   return (
     <div className='container'>
@@ -104,7 +104,7 @@ const hoverEffectClass = 'hover-effect';
                       <p className='artist b2'>{leftColumn.musicPlayer?.artistAuthor}</p>
                       <p className='track-title b2'>{leftColumn.musicPlayer?.trackTitle}</p>
                     </div>
-                    <audio controls className='audio-player custom-audio-player'>
+                    <audio controls="true" className='audio-player custom-audio-player'>
                       <source src={leftColumn.musicPlayer?.audio?.url} type='audio/mp3' />
                       Your browser does not support the audio tag.
                     </audio>
@@ -118,11 +118,11 @@ const hoverEffectClass = 'hover-effect';
         <div className='right-column col-lg-7 offset-1'>
           {rightHarmonyColumn?.musicCollection?.map((collection, index) => (
             <div key={index} className='container music-collection-item'>
-              <div className='row music-row'>
-                <div className='col-md-6 music-left-column'>
+              <div className='row music-row mb-3'>
+                <div className='col-4 col-md-6 music-left-column'>
                   <img src={collection?.image?.sourceUrl} alt={collection?.image?.altText || ''} className='collection-image' />
                 </div>
-                <div className='col-md-6 music-right-column'>
+                <div className='col-8 col-md-6 music-right-column'>
                   <div className='content-container'>
                     <div className='d-flex flex-column justify-content-center h-100'> {/* Center vertically */}
                       <Heading level='h4' className='music-head text-left'>{collection?.title}</Heading>
@@ -130,33 +130,71 @@ const hoverEffectClass = 'hover-effect';
                         className='blurb text-left b3'
                         dangerouslySetInnerHTML={{ __html: collection?.description || '' }}
                       />
-                      {collection?.musicButton?.some(button => button.icon && button.link) && (
-                        <div className='music-container mt-4'>
+                      {collection?.spotifyLink?.url && (
+                        <div className='music-container mt-4 d-md-block d-none'>
                           <div className='music-buttons-row'>
                             <h5>Listen Now:</h5>
-                            {/* {collection.musicButton.map((button, buttonIndex) => {
-                              if (button.icon && button.link) {
-                                return (
-                                  <div key={buttonIndex} className={`music-button ${hoverEffectClass}`}>
-                                  <button onClick={() => window.location.href = button.link.url} style={{ padding: 0 }}>
-                                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
-                                        <circle cx="25" cy="25" r="24.5" stroke="#373A36" fill="#FFF" stroke-opacity="0.2" />
-                                        <image href={button.icon.sourceUrl} x="15" y="15" width="20" height="20" />
-                                      </svg>
+                            {collection.spotifyLink?.url && (
+                              <div className={`music-button ${hoverEffectClass}`}>
+                                <button onClick={() => window.location.href = collection.spotifyLink.url} style={{ padding: 0 }}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+                                    <circle cx="25" cy="25" r="24.5" stroke="#373A36" fill="#FFF" strokeOpacity="0.2" />
+                                    <image xlinkHref="https://settings.primroseschools.com/wp-content/uploads/2024/01/Object.png" x="15" y="15" width="20" height="20" />
+                                  </svg>
+                                  {collection.spotifyLink.title}
+                                </button>
+                              </div>
+                            )}
 
-                                      {button.link.title}
-                                    </button>
-                                  </div>
-                                );
-                              } else {
-                                return null;
-                              }
-                            })} */}
+                            {collection.appleMusicLink?.url && (
+                              <div className={`music-button ${hoverEffectClass}`}>
+                                <button onClick={() => window.location.href = collection.appleMusicLink.url} style={{ padding: 0 }}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+                                    <circle cx="25" cy="25" r="24.5" stroke="#373A36" fill="#FFF" strokeOpacity="0.2" />
+                                    <image xlinkHref="https://settings.primroseschools.com/wp-content/uploads/2024/01/Object2.png" x="15" y="15" width="20" height="20" />
+                                  </svg>
+                                  {collection.appleMusicLink.title}
+                                </button>
+                              </div>
+                            )}
                           </div>
                         </div>
                       )}
                     </div>
                   </div>
+                </div>
+                <div className="col-12 d-block d-md-none"> 
+                {collection?.spotifyLink?.url && (
+                        <div className='music-container mt-4'>
+                          <div className='music-buttons-row'>
+                            <h5>Listen Now:</h5>
+                            {collection.spotifyLink?.url && (
+                              <div className={`music-button ${hoverEffectClass}`}>
+                                <button onClick={() => window.location.href = collection.spotifyLink.url} style={{ padding: 0 }}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+                                    <circle cx="25" cy="25" r="24.5" stroke="#373A36" fill="#FFF" strokeOpacity="0.2" />
+                                    <image xlinkHref="https://settings.primroseschools.com/wp-content/uploads/2024/01/Object.png" x="15" y="15" width="20" height="20" />
+                                  </svg>
+                                  {collection.spotifyLink.title}
+                                </button>
+                              </div>
+                            )}
+
+                            {collection.appleMusicLink?.url && (
+                              <div className={`music-button ${hoverEffectClass}`}>
+                                <button onClick={() => window.location.href = collection.appleMusicLink.url} style={{ padding: 0 }}>
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+                                    <circle cx="25" cy="25" r="24.5" stroke="#373A36" fill="#FFF" strokeOpacity="0.2" />
+                                    <image xlinkHref="https://settings.primroseschools.com/wp-content/uploads/2024/01/Object2.png" x="15" y="15" width="20" height="20" />
+                                  </svg>
+                                  {collection.appleMusicLink.title}
+                                </button>
+                              </div>
+                            )}
+                          </div>
+                        </div>
+                      )}
+
                 </div>
               </div>
             </div>
