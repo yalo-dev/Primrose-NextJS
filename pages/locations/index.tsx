d5a1dac8e7621591a32e73813ffdc9c47480377e
Elijah Williams <williamselijah527@gmail.com>
2024-02-05 11:30:30 -0500

    list schools in alphabetical order

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -16,7 +16,7 @@ export async function getStaticProps() {
     try {
         const SCHOOLS_QUERY = gql`
         query GetSchools {
-            schools(first: 1000) {
+            schools(first: 1000, where: {orderby: {field: TITLE, order: ASC}}) {
                 nodes {
                   markets {
                     nodes {
@@ -119,7 +119,7 @@ export default function Locations({ markets, schools }) {
     return (
         
         <>
-            <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous" />
+            <Script strategy="beforeInteractive" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" />
             <div className='container-fluid bg-gray heading py-5'>
                 <div className='row py-5 '>
                     <div className='col-12 text-center'>
