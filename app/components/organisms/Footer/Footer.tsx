6aeab829226e294b41758523d7583cd51a52356c
Nick Black <nick.black@digitalyalo.com>
2024-02-13 11:41:30 -0800

    tweak to footer onetrust implementation

--- app/components/organisms/Footer/Footer.tsx
+++ app/components/organisms/Footer/Footer.tsx
@@ -71,7 +71,7 @@ export default function Footer({ menuItems, siteSettings }) {
                             )}
                         </li>
                     ))}
-                    <li className="ot-sdk-show-settings p-2 ps-0"><a href="#">Do Not Sell or Share My Personal Information</a></li>
+                    <li className="ot-sdk-show-settings p-2 ps-0 pe-auto">Do Not Sell or Share My Personal Information</li>
                 </ul>
                 <div className='bottom col-12 col-lg-6 text-left mt-4 order-5'>
                     <div className='text col-12' dangerouslySetInnerHTML={{ __html: copyrightInfo }}></div>
