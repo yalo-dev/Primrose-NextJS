3bc221e7149898024c33364a42beff53bfe8c0e7
Nick Black <nick.black@digitalyalo.com>
2024-02-13 09:22:48 -0800

    installed optimizely and one trust directly to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -43,13 +43,16 @@ class MyDocument extends Document {
                             `,
                         }}
                     />
-                    <Script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" data-domain-script="dcc6852c-83ad-4770-8fe8-5c1528352fce"></Script>
-                    <Script>
-                        {`
-                           function OptanonWrapper() { }
-                        `}
-                    </Script>
-                    <Script src="https://cdn.optimizely.com/js/20299544930.js"></Script>
+                    <Script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" data-domain-script="dcc6852c-83ad-4770-8fe8-5c1528352fce" />
+                     <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            function OptanonWrapper() { }
+                            `,
+                        }}
+                    />
+                    <Script src="https://cdn.optimizely.com/js/20299544930.js" />
+                   
                     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
                     <link
                         href="//fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@200;400&family=Poppins:wght@300;400;500&display=swap"
