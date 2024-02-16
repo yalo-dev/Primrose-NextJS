667a056fc1cdc705c1a21843af80bfd0646a5bad
Nick Black <nick.black@digitalyalo.com>
2024-02-16 10:29:23 -0800

    fixed nav item spacing

--- app/components/organisms/Header/Header.tsx
+++ app/components/organisms/Header/Header.tsx
@@ -116,7 +116,7 @@ export default function Header({ menuItems }) {
                 </Link>
                 <div className='parent-item nav-link d-flex d-lg-none justify-content-between' onClick={() => toggleSubmenu(key)}>
                     {item.label}
-                    <span className="arrow pe-3">
+                    <span className="arrow px-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="6" height="12" viewBox="0 0 6 12" fill="none">
                             <path fillRule="evenodd" clipRule="evenodd" d="M0.323475 11.794C-0.0579244 11.4797 -0.109455 10.9192 0.208378 10.542L4.20212 5.80315L0.233801 1.48682C-0.100162 1.12357 -0.0730885 0.561399 0.29427 0.231171C0.661629 -0.0990572 1.23016 -0.0722866 1.56413 0.290963L5.53244 4.60729C6.13597 5.26375 6.15767 6.25971 5.58329 6.94125L1.58955 11.6801C1.27172 12.0573 0.704875 12.1082 0.323475 11.794Z" fill="white" />
                         </svg>
