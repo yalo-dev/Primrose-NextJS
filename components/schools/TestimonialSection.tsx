9c12bde1ddb239d7d6f8f9be8c90f22dcfae396e
Chris Dawson <chris.dawson@digitalyalo.com>
2024-02-20 09:38:21 -0500

    add limit of 5 to testimonials

--- components/schools/TestimonialSection.tsx
+++ components/schools/TestimonialSection.tsx
@@ -11,7 +11,7 @@ export default function TestimonialSection({adminSettings}: TestimonialSectionPr
 
     if (!testimonials) return
 
-    const transformedTestimonials = adminSettings?.testimonials?.filter(testimonial => testimonial).map(testimonial => ({
+    const transformedTestimonials = adminSettings?.testimonials?.filter(testimonial => testimonial).slice(0,5).map(testimonial => ({
         avatar: {
             sourceUrl: testimonial.featuredImage?.node?.sourceUrl,
             altText: 'Testimonial'
