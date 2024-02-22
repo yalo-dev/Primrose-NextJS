a970647ceec6a8dfe13ae7e53389c4d523bb56b1
AndrewSmithDY <andrew.smith@digitalyalo.com>
2024-02-22 14:23:12 -0600

    add 404 if classroom type is not offered by school. remove unused inline satSlider

--- pages/schools/[schoolSlug]/classrooms/[classroomType]/index.tsx
+++ pages/schools/[schoolSlug]/classrooms/[classroomType]/index.tsx
@@ -446,8 +446,7 @@ export async function getServerSideProps(context) {
         classroomId: `${classroomType}`
       },
     });
-    
-    if (!response || !response.data || !response.data.school) {
+    if (!response || !response.data || !response.data.school || !response.data.classroom) {
       return { notFound: true };
     }
     
@@ -572,93 +571,6 @@ export default function ClassroomTypePage({ school, schoolSlug, data }) {
         </>
     )
 }
-  const findASchool = () => {
-
-    const ScheduleATour = school?.schoolAdminSettings[camelize(classroomType) as string]?.classroomGallery ? school?.schoolAdminSettings[camelize(classroomType) as string].classroomGallery : school?.schoolAdminSettings?.satImages ;
-    const hasScheduleATour = ScheduleATour;
-    const leftScrollerRef = useRef<HTMLDivElement>(null);
-    const rightScrollerRef = useRef<HTMLDivElement>(null);
-    const [allImagesLoaded, setAllImagesLoaded] = useState(false);
-
-    const scrollContent = () => {
-      const leftScroller = leftScrollerRef.current;
-      const rightScroller = rightScrollerRef.current;
-
-      if (leftScroller) {
-        leftScroller.scrollTop += 1;
-        if (leftScroller.scrollTop >= leftScroller.scrollHeight / 2) {
-          leftScroller.scrollTop = 0;
-        }
-      }
-
-      if (rightScroller) {
-        rightScroller.scrollTop -= 1;
-        if (rightScroller.scrollTop <= 0) {
-          rightScroller.scrollTop = rightScroller.scrollHeight / 2;
-        }
-      }
-    };
-
-    useEffect(() => {
-      const checkIfImagesLoaded = () => {
-        const images = document.querySelectorAll('.find-a-school .image-scroller img');
-        return Array.from(images).every((img) => (img as HTMLImageElement).complete);
-      };
-      if (checkIfImagesLoaded()) {
-        setAllImagesLoaded(true);
-        setInterval(scrollContent, 20);
-      } else {
-        const images = document.querySelectorAll('.find-a-school .image-scroller img');
-        images.forEach((img) => {
-          img.addEventListener('load', () => {
-            if (checkIfImagesLoaded()) {
-              setAllImagesLoaded(true);
-              setInterval(scrollContent, 20);
-            }
-          });
-        });
-      }
-    }, []);
-    if (!hasScheduleATour) return null;
-    return (
-
-      <div className='container'>
-        <div className='find-a-school'>
-          <div className='left-column col-8 col-lg-7 col-xxl-6 d-lg-flex flex-lg-column justify-content-lg-center'>
-           <Heading level='h2'>Our family would love to meet yours.</Heading>
-             <Subheading level='div' className='b3'>Contact us to schedule a tour.</Subheading>
-            
-             <Button variant="secondary" href={"/schools/" + schoolSlug + "/schedule-a-tour"}>
-                  Schedule A Tour
-              </Button>
-            
-          </div>
-          <div className='right-column col-4 col-lg-5 col-xxl-6'>
-            {ScheduleATour && ScheduleATour.length > 0 && (
-              <>
-                <div className="image-scroller first" ref={leftScrollerRef}>
-                  {ScheduleATour.map((imgObj, idx) => (
-                    imgObj.image.sourceUrl && <img key={idx} src={imgObj.image.sourceUrl} alt={imgObj.altText || 'slider image'} />
-                  ))}
-                  {ScheduleATour.map((imgObj, idx) => (
-                    imgObj.image.sourceUrl && <img key={`dup-${idx}`} src={imgObj.image.sourceUrl} alt={imgObj.altText || 'slider image'} />
-                  ))}
-                </div>
-                <div className="image-scroller second" ref={rightScrollerRef}>
-                  {ScheduleATour.map((imgObj, idx) => (
-                    imgObj.image.sourceUrl && <img key={idx} src={imgObj.image.sourceUrl} alt={imgObj.altText || 'slider image'} />
-                  ))}
-                  {ScheduleATour.map((imgObj, idx) => (
-                    imgObj.image.sourceUrl && <img key={`dup-${idx}`} src={imgObj.image.sourceUrl} alt={imgObj.altText || 'slider image'} />
-                  ))}
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    )
-  }
   const BussingInformation = () => {
     let bussingInformation = school?.schoolAdminSettings[camelize(classroomType) as string]?.bussingInformation;
     if(!bussingInformation){
