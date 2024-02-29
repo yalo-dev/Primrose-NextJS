69151a5b4f8ea40f957e993df578c4aff29ff253
AndrewSmithDY <andrew.smith@digitalyalo.com>
2024-02-29 15:09:41 -0600

    prevent scroll interval from compiling on rerenders

--- components/schools/ScheduleATourSlider.tsx
+++ components/schools/ScheduleATourSlider.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef} from "react";
 import Heading from "../../app/components/atoms/Heading/Heading";
 import Subheading from "../../app/components/atoms/Subheading/Subheading";
 import Button from "../../app/components/atoms/Button/Button";
@@ -12,43 +12,27 @@ export default function ScheduleATourSlider({images, schoolSlug }: ScheduleATour
     if (!images) return
 
     const scheduleATourImages = images?.length ? images : null;
-    const leftScrollerRef = useRef<HTMLDivElement>(null);
+    const scrollRef = useRef<HTMLDivElement>(null);
 
     const scrollContent = () => {
-        const leftScroller = leftScrollerRef.current;
+        const scroller = scrollRef.current;
 
-        if (leftScroller) {
-            leftScroller.scrollTop += 1;
-            if (leftScroller.scrollTop >= leftScroller.scrollHeight / 2) {
-                leftScroller.scrollTop = 0;
+        if (scroller) {
+            scroller.scrollTop += 1;
+            if (scroller.scrollTop >= scroller.scrollHeight / 2) {
+                scroller.scrollTop = 0;
             }
         }
     };
-
     useEffect(() => {
-        const checkIfImagesLoaded = () => {
-            const images = document.querySelectorAll('.find-a-school .image-scroller img');
-            return Array.from(images).every((img) => (img as HTMLImageElement).complete);
-        };
-        if (checkIfImagesLoaded()) {
-            setInterval(scrollContent, 20);
-        } else {
-            const images = document.querySelectorAll('.find-a-school .image-scroller img');
-            images.forEach((img) => {
-                img.addEventListener('load', () => {
-                    if (checkIfImagesLoaded()) {
-                        setInterval(scrollContent, 20);
-                    }
-                });
-            });
-        }
+        const scrollInterval = setInterval(scrollContent, 10);
+        return () => clearInterval(scrollInterval)
     }, []);
-    return (
 
+    return (
         <div className='container'>
             <div className='find-a-school'>
-                <div
-                    className='left-column col-8 col-lg-7 col-xxl-6 d-lg-flex flex-lg-column justify-content-lg-center'>
+                <div className='left-column col-8 col-lg-7 col-xxl-6 d-lg-flex flex-lg-column justify-content-lg-center'>
                     <Heading level='h2'>Our family would love to meet yours.</Heading>
                     <Subheading level='div' className='b3'>Contact us to schedule a tour.</Subheading>
                     <Button variant="secondary" href={"/schools/" + schoolSlug + "/schedule-a-tour"}>
@@ -56,7 +40,7 @@ export default function ScheduleATourSlider({images, schoolSlug }: ScheduleATour
                     </Button>
                 </div>
                 <div className='right-column col-4 col-lg-5 col-xxl-6'>
-                    <div className="image-scroller first" ref={leftScrollerRef}>
+                    <div className="image-scroller first" ref={scrollRef}>
                         {scheduleATourImages.map((imgObj, idx) => (
                             imgObj.url && <img key={idx} src={imgObj.url}
                                                            alt={imgObj.altText || `slider image ${idx}`}/>
