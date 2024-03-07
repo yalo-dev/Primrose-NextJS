3713d61c41a8c7b1801a22ffe7c808b88d509d52
AndrewSmithDY <andrew.smith@digitalyalo.com>
2024-03-07 11:51:05 -0600

    remove slack webhook

--- app/components/organisms/CustomError.tsx
+++ app/components/organisms/CustomError.tsx
@@ -8,21 +8,7 @@ interface CustomErrorProps {
     message: string
 }
 export default function CustomError({statusCode, message}: CustomErrorProps) {
-    const router = useRouter()
-    useEffect(() => {
-        // don't alert dev team in development environments or 404 errors
-        if (statusCode !== 404 && process.env.NODE_ENV !== 'development') {
-            axios.post(
-                'https://hooks.slack.com/services/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',
-                {text: `ERROR ${statusCode} ON PATH '${router.asPath}' MESSAGE -> ${message}`}
-            ).then(function (response) {
-                console.error("Development team has been notified of this error, and will be looking to resolve it as soon as possible")
-            }).catch(function (error) {
-                console.error("There was an error notifying the dev them")
-            });
-        }
 
-    }, []);
 
     return (
         <div className='container col-lg-10 offset-lg-1 pt-4'>
---
+++ pages/test-error-page.tsx
@@ -0,0 +1,4 @@
+export default function () {
+    const test = null
+    return <h1>{test.test}</h1>
+}
