d2b70f9db8d97de286f3c918c6f1bedb23d2f356
Chris Dawson <chris.dawson@digitalyalo.com>
2024-03-25 13:33:04 -0400

    remove phone icon from desktop view

--- app/components/organisms/SchoolsMenu/SchoolsMenu.tsx
+++ app/components/organisms/SchoolsMenu/SchoolsMenu.tsx
@@ -170,7 +170,7 @@ export default function SchoolsMenu() {
               <div className='col w-100 col d-flex align-items-center justify-content-start'>
                 <Button label='Schedule a Tour' className={'schools-menu'} href={`/schools/${schoolSlug}/schedule-a-tour`} />
                 {schoolPhone && (
-                  <div className='phone ps-2'>
+                  <div className='phone ps-2 d-lg-none'>
                   <Link href={`tel:${schoolPhone}`}>
                     <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <circle cx="25" cy="25" r="24.5" fill="white" stroke="#DFE2D3" />
