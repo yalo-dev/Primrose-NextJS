07093d234016ba4ac7c7efab1b916e0de798c0a5
Chris Dawson <chris.dawson@digitalyalo.com>
2024-05-20 15:25:02 -0400

    remove commented out component that isn't used

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -232,7 +232,6 @@ export default function Document() {
                             })();`,
           }}
         ></Script>
-        {/*<DynamicComponentWithNoSSR />*/}
         <Main />
         <NextScript />
       </body>
